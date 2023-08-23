281596c61caefc0bcc14ffd08941635074171d62
DamonCharlesRoberts <damon.charles.roberts@gmail.com>
2023-08-23 11:43:27 -0600

    doing some editing

--- chapter_2/src/models/study_1_fitting.R
+++ chapter_2/src/models/study_1_fitting.R
@@ -59,14 +59,28 @@ list_ppc <- list(
 
 # Hypothesis 2
     #* Fit models
-list_fitted[["h_2"]][["red"]] <- brm(
+list_fitted[["h_2"]][["red_multinomial"]] <- brm(
+  formula = trial_1_party ~ trial_1_red_stimuli
+  , data = list_df[["cleaned"]]
+  , family = categorical(link = "logit")
+  , prior = set_prior("normal(0, 1)", class = "b")
+  , backend = "cmdstanr"
+)
+list_fitted[["h_2"]][["blue_multinomial"]] <- brm(
+  formula = trial_1_party ~ trial_1_blue_stimuli
+  , data = list_df[["cleaned"]]
+  , family = categorical(link = "logit")
+  , prior = set_prior("normal(0, 1)", class = "b")
+  , backend = "cmdstanr"
+)
+list_fitted[["h_2"]][["red_ordinal"]] <- brm(
   formula = trial_1_party ~ trial_1_red_stimuli
   , data = list_df[["cleaned"]]
   , family = cumulative(link = "logit")
   , prior = set_prior("normal(0, 1)", class = "b")
   , backend = "cmdstanr"
 )
-list_fitted[["h_2"]][["blue"]] <- brm(
+list_fitted[["h_2"]][["blue_ordinal"]] <- brm(
   formula = trial_1_party ~ trial_1_blue_stimuli
   , data = list_df[["cleaned"]]
   , family = cumulative(link = "logit")
@@ -109,11 +123,17 @@ list_fitted[["h_2"]][["blue_colorblind"]] <- brm(
   , backend = "cmdstanr"
 )
     #* Posterior predictive checks
-list_ppc[["h_2"]][["red"]] <- pp_check(
-  list_fitted[["h_2"]][["red"]]
+list_ppc[["h_2"]][["red_multinomial"]] <- pp_check(
+  list_fitted[["h_2"]][["red_multinomial"]]
 )
-list_ppc[["h_2"]][["blue"]] <- pp_check(
-  list_fitted[["h_2"]][["blue"]]
+list_ppc[["h_2"]][["blue_multinomial"]] <- pp_check(
+  list_fitted[["h_2"]][["blue_multinomial"]]
+)
+list_ppc[["h_2"]][["red_ordinal"]] <- pp_check(
+  list_fitted[["h_2"]][["red_ordinal"]]
+)
+list_ppc[["h_2"]][["blue_ordinal"]] <- pp_check(
+  list_fitted[["h_2"]][["blue_ordinal"]]
 )
 list_ppc[["h_2"]][["single_model"]] <- pp_check(
   list_fitted[["h_2"]][["single_model"]]
@@ -132,14 +152,28 @@ list_ppc[["h_2"]][["blue_colorblind"]] <- pp_check(
 )
 # Hypothesis 2a
     #* Fit the models
-list_fitted[["h_2a"]][["trial_2_red"]] <- brm(
+list_fitted[["h_2a"]][["trial_2_red_multinomial"]] <- brm(
+  formula = trial_2_party ~ trial_2_red_stimuli
+  , data = list_df[["cleaned"]]
+  , family = categorical(link = "logit")
+  , prior = set_prior("normal(0, 1)", class = "b")
+  , backend = "cmdstanr"
+)
+list_fitted[["h_2a"]][["trial_2_blue_multinomial"]] <- brm(
+  formula = trial_2_party ~ trial_2_blue_stimuli
+  , data = list_df[["cleaned"]]
+  , family = categorical(link = "logit")
+  , prior = set_prior("normal(0, 1)", class = "b")
+  , backend = "cmdstanr"
+)
+list_fitted[["h_2a"]][["trial_2_red_ordinal"]] <- brm(
   formula = trial_2_party ~ trial_2_red_stimuli
   , data = list_df[["cleaned"]]
   , family = cumulative(link = "logit")
   , prior = set_prior("normal(0, 1)", class = "b")
   , backend = "cmdstanr"
 )
-list_fitted[["h_2a"]][["trial_2_blue"]] <- brm(
+list_fitted[["h_2a"]][["trial_2_blue_ordinal"]] <- brm(
   formula = trial_2_party ~ trial_2_blue_stimuli
   , data = list_df[["cleaned"]]
   , family = cumulative(link = "logit")
@@ -153,14 +187,28 @@ list_fitted[["h_2a"]][["trial_2_single_model"]] <- brm(
   , prior = set_prior("normal(0, 1)", class = "b")
   , backend = "cmdstanr"
 )
-list_fitted[["h_2a"]][["trial_3_red"]] <- brm(
+list_fitted[["h_2a"]][["trial_3_red_multinomial"]] <- brm(
+    formula = trial_3_party ~ trial_3_red_stimuli
+    , data = list_df[["cleaned"]]
+    , family = categorical(link = "logit")
+    , prior = set_prior("normal(0, 1)", class = "b")
+    , backend = "cmdstanr"
+)
+list_fitted[["h_2a"]][["trial_3_blue_multinomial"]] <- brm(
+    formula = trial_3_party ~ trial_3_blue_stimuli
+    , data = list_df[["cleaned"]]
+    , family = categorical(link = "logit")
+    , prior = set_prior("normal(0, 1)", class = "b")
+    , backend = "cmdstanr"
+)
+list_fitted[["h_2a"]][["trial_3_red_ordinal"]] <- brm(
     formula = trial_3_party ~ trial_3_red_stimuli
     , data = list_df[["cleaned"]]
     , family = cumulative(link = "logit")
     , prior = set_prior("normal(0, 1)", class = "b")
     , backend = "cmdstanr"
 )
-list_fitted[["h_2a"]][["trial_3_blue"]] <- brm(
+list_fitted[["h_2a"]][["trial_3_blue_ordinal"]] <- brm(
     formula = trial_3_party ~ trial_3_blue_stimuli
     , data = list_df[["cleaned"]]
     , family = cumulative(link = "logit")
@@ -175,20 +223,32 @@ list_fitted[["h_2a"]][["trial_3_single_model"]] <- brm(
   , backend = "cmdstanr"
 )
     #* Posterior predictive checks
-list_ppc[["h_2a"]][["trial_2_red"]] <- pp_check(
-  list_fitted[["h_2a"]][["trial_2_red"]]
+list_ppc[["h_2a"]][["trial_2_red_multinomial"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_2_red_multinomial"]]
 )
-list_ppc[["h_2a"]][["trial_2_blue"]] <- pp_check(
-  list_fitted[["h_2a"]][["trial_3_blue"]]
+list_ppc[["h_2a"]][["trial_2_blue_multinomial"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_3_blue_multinomial"]]
+)
+list_ppc[["h_2a"]][["trial_2_red_ordinal"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_2_red_ordinal"]]
+)
+list_ppc[["h_2a"]][["trial_2_blue_ordinal"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_3_blue_ordinal"]]
 )
 list_ppc[["h_2a"]][["trial_2_single_model"]] <- pp_check(
   list_fitted[["h_2a"]][["trial_2_single_model"]]
 )
-list_ppc[["h_2a"]][["trial_3_red"]] <- pp_check(
-  list_fitted[["h_2a"]][["trial_3_red"]]
+list_ppc[["h_2a"]][["trial_3_red_multinomial"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_3_red_multinomial"]]
 )
-list_ppc[["h_2a"]][["trial_3_blue"]] <- pp_check(
-  list_fitted[["h_2a"]][["trial_3_blue"]]
+list_ppc[["h_2a"]][["trial_3_blue_multinomial"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_3_blue_multinomial"]]
+)
+list_ppc[["h_2a"]][["trial_3_red_ordinal"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_3_red_ordinal"]]
+)
+list_ppc[["h_2a"]][["trial_3_blue_ordinal"]] <- pp_check(
+  list_fitted[["h_2a"]][["trial_3_blue_ordinal"]]
 )
 list_ppc[["h_2a"]][["trial_3_single_model"]] <- pp_check(
   list_fitted[["h_2a"]][["trial_3_single_model"]]
