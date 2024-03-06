f213860a5fb8f785d03e60db3ae01e6ac728a685
DamonCharlesRoberts <damon.charles.roberts@gmail.com>
2024-03-06 18:53:59 -0500

    adding cleaning scripts for ch2 st2

---
+++ chapter_3/src/cleaning/data-loading.R
@@ -0,0 +1,30 @@
+# Loading data file
+
+# Notes
+# *     Description
+# **        Script that loads and stores the data into a DB
+
+# Load libraries
+box::use(
+    haven[read_sav],
+    duckdb[...],
+    DBI[dbConnect,dbExecute]
+)
+
+# Connect to the DB
+con <- dbConnect(duckdb(), dbdir = "./chapter_3/data/clean/study_1.db")
+
+# Load the file
+df <- read_sav(
+    "./chapter_3/data/original/CCES23_CUB_OUTPUT.sav"
+)
+df_common <- read_sav(
+    "./chapter_3/data/original/CCES23_COMMON_OUTPUT.sav"
+)
+
+# Store the data in the DB
+dbWriteTable(con, "OriginalModule", df)
+dbWriteTable(con, "OriginalCommon", df_common)
+
+# Disconnect from the database
+dbDisconnect(con)
